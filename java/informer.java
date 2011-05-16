68f6b56e819316f9a2de7a4ef29bc6d0af5d1c67
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-05-16 21:32:28 +0000

    Changed Java listener and informer to use events of varying size * java/informer.java: send events of sizes 4, 256, 400000 like the   other languages; use scopes /sizeSIZE/sub1/sub2 * java/listener.java: similar to informer

--- java/informer.java
+++ java/informer.java
@@ -1,29 +1,45 @@
+import java.lang.StringBuilder;
+
+import java.util.ArrayList;
 
 import rsb.Publisher;
 import rsb.InitializeException;
 
 public class informer {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		try	{
-		
-			Publisher<String> p = new Publisher<String>("/example/informer");
-			p.activate();
-			
-			for (int j = 1; j <= 1200; j++) {
-				p.send("This is a test message " + j);
-			}
-			
-			System.out.println("Sent messages");
-			
-			p.deactivate();
-			
-		} catch(InitializeException e) {
-			e.printStackTrace();
-			System.exit(1);
+	ArrayList<Integer> sizes = new ArrayList<Integer>();
+	sizes.add(4);
+	sizes.add(256);
+	sizes.add(400000);
+	for (int size : sizes) {
+	    String scope = "/size" + size + "/sub1/sub2";
+
+	    System.out.println("[Java   Informer] Processing scope " + scope);
+	    try {
+		Publisher<String> p = new Publisher<String>(scope);
+		p.activate();
+
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < size; ++i) {
+		    builder.append('c');
 		}
-		
+		String data = builder.toString();
+
+		for (int j = 1; j <= 1200; j++) {
+		    p.send(data);
+		}
+
+		System.out.println("[Java   Informer] Sent messages");
+
+		p.deactivate();
+
+	    } catch(InitializeException e) {
+		e.printStackTrace();
+		System.exit(1);
+	    }
 	}
+    }
 
 }
--- java/listener.java
+++ java/listener.java
@@ -1,66 +1,69 @@
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.TimeUnit;
+import java.lang.Integer;
 
-import rsb.RSBException;
+import java.util.ArrayList;
+
 import rsb.Subscriber;
 import rsb.event.RSBDataListener;
 import rsb.event.Subscription;
 import rsb.transport.TransportFactory;
-import rsb.InitializeException;
-import rsb.naming.NotFoundException;
 
 public class listener {
 
-	private static class RSBQueueAdapter<T> extends RSBDataListener<T> {
-		BlockingQueue<T> queue;
-
-		public RSBQueueAdapter() {
-			this(new LinkedBlockingDeque<T>());
-		}
-
-		public RSBQueueAdapter(BlockingQueue<T> queue) {
-			this.queue = queue;
-		}
-
-		@Override
-		public void handleEvent(T data) {
-			queue.add(data);
-		}
-
-		public BlockingQueue<T> getQueue() {
-			return queue;
-		}
+    private static class DataHandler<T> extends RSBDataListener<T> {
+	public DataHandler(String scope, int size, int expected) {
+	    this.scope = scope;
+	    this.size = size;
+	    this.expected = expected;
 	}
 
-	public static void main(String[] args) {
+	@Override
+        public void handleEvent(T data) {
+	    this.count++;
+	    System.out.println("[Java  Listener] Scope " + scope + ": received "
+			       + this.count + "/" + this.expected + ": " + data);
+	}
 
+	public boolean isDone() {
+	    return this.count == this.expected;
+	}
+
+	private String scope;
+	private int size;
+	private int count;
+	private int expected;
+    }
+
+    public static void main(String[] args) {
+
+	ArrayList<Integer> sizes = new ArrayList<Integer>();
+	sizes.add(4);
+	sizes.add(256);
+	sizes.add(400000);
+	ArrayList<String> components = new ArrayList<String>();
+	components.add("/");
+	components.add("sub1");
+	components.add("/sub2");
+	for (int size : sizes) {
+	    String scope = "/size" + size;
+	    for (String component : components) {
+		scope += component;
 		try {
+		    Subscriber subscriber = new Subscriber(scope, scope,
+							   TransportFactory.getInstance());
+		    subscriber.activate();
+		    DataHandler<String> handler = new DataHandler<String>(scope, size, 1200);
+		    Subscription subscription = subscriber.addListener(handler);
 
-			Subscriber subscriber = new Subscriber("/example/informer",
-					"/example/informer", TransportFactory.getInstance());
-			subscriber.activate();
-			RSBQueueAdapter<String> queueAdapter = new RSBQueueAdapter<String>();
-			Subscription subscription = subscriber.addListener(queueAdapter);
-			BlockingQueue<String> inputQueue = queueAdapter.getQueue();
+		    while (!handler.isDone()) {
+			Thread.sleep(10);
+		    }
 
-			for (int j = 1; j <= 1200; j++) {
-				String message = inputQueue.poll(10000, TimeUnit.MILLISECONDS);
-				if (message == null) {
-					System.err.println("No messages received in 10 seconds");
-					System.exit(1);
-				} else {
-					System.out.println("Received " + j + "/1200: " + message);
-				}
-			}
-			
-			subscriber.deactivate();
-
-		} catch (Throwable e) {
-			e.printStackTrace();
-			System.exit(1);
+		    subscriber.deactivate();
+		} catch (java.lang.Throwable e) {
+		    System.err.println("[Java Listener] Failure for size " + size + ": " + e);
 		}
-
+	    }
 	}
+    }
 
 }
