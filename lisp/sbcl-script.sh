ab7a538283e35fb84950a5b0d4c6a32fbcf33ef1
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-05-17 21:11:53 +0000

    Use distribution-provided version of SBCL * lisp/sbcl-script.sh: use distribution-provided version of SBCL

--- lisp/sbcl-script.sh
+++ lisp/sbcl-script.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
 # Lisp Version
-sbcl_version="1.0.47"
-if [ $(uname -m) == "i686" ] ; then
-  sbcl_home="/vol/cl/sbcl-${sbcl_version}/lib/sbcl"
-  sbcl_bin="/vol/cl/sbcl-${sbcl_version}/bin/sbcl"
-else
-  echo "No appropriate SBCL installation for arch $(uname -m)"
-  exit 1
-fi
+sbcl_version=$(sbcl --version | cut -c 6-)
+
 
 # ASDF Setup
 export CL_SOURCE_REGISTRY="${WORKSPACE}//:"
 export ASDF_OUTPUT_TRANSLATIONS="(:output-translations (t \"${WORKSPACE}/../.fasl-cache-${sbcl_version}-${JOB_NAME}\") :ignore-inherited-configuration)"
 
 # Set home and execute code
-export SBCL_HOME="${sbcl_home}"
-"${sbcl_bin}" --script "$1"
+sbcl --script "$1"
