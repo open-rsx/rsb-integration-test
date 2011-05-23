b39f8365cf1e1e18dc09e1646340eb55d61a21b4
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-05-23 18:57:23 +0000

    Pass arguments to Lisp in lisp/sbcl-script.sh * lisp/sbcl-script.sh: pass arguments following script name to Lisp   process

--- lisp/sbcl-script.sh
+++ lisp/sbcl-script.sh
@@ -9,4 +9,4 @@ export CL_SOURCE_REGISTRY="${WORKSPACE}//:"
 export ASDF_OUTPUT_TRANSLATIONS="(:output-translations (t \"${WORKSPACE}/../.fasl-cache-${sbcl_version}-${JOB_NAME}\") :ignore-inherited-configuration)"
 
 # Set home and execute code
-sbcl --script "$1"
+sbcl --script "$@"
