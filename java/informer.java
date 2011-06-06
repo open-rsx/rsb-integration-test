51a90520fbc53380e31629d0bcddeff1a62bb1eb
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-06-06 16:46:33 +0000

    Added sending of meta-data to Java informer fixes #349 * java/informer.java: add "informer-lang" and "index" meta-info items;   add "informer-start" user-time * java/informer.sh.in: pass commandline arguments to Java program * java/listener.sh.in: likewise

--- java/informer.java
+++ java/informer.java
@@ -1,15 +1,18 @@
-import java.lang.StringBuilder;
-
 import java.util.ArrayList;
 
 import rsb.Factory;
 import rsb.Informer;
 import rsb.InitializeException;
 import rsb.Scope;
+import rsb.MetaData;
+import rsb.Event;
 
 public class informer {
 
     public static void main(String[] args) {
+
+        long startTime = System.currentTimeMillis() * 1000;
+        int listenerPid = Integer.parseInt(args[1]);
 
         ArrayList<Integer> sizes = new ArrayList<Integer>();
         sizes.add(4);
@@ -29,8 +32,14 @@ public class informer {
                 }
                 String data = builder.toString();
 
+                Event event = new Event("string", data);
+                MetaData metaData = event.getMetaData();
+                metaData.setUserInfo("informer-lang", "Java");
+                metaData.setUserTime("informer-start", startTime);
+
                 for (int j = 1; j <= 120; j++) {
-                    p.send(data);
+                    metaData.setUserInfo("index", "" + (listenerPid + j));
+                    p.send(event);
                 }
 
                 System.out.println("[Java   Informer] Sent messages");
