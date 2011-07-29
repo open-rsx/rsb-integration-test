475507ff6e10e062c759a24f67c053611bb444d5
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2011-07-29 21:39:04 +0000

    Created stubs for Lisp client/server test in integrationtest/lisp refs #444 * lisp/client.lisp: new file; contains Lisp client for client/server   integration test * lisp/server.lisp: new file; contains Lisp server for client/server   integration test

---
+++ lisp/client.lisp
@@ -0,0 +1,45 @@
+(load-system :sb-posix)
+(load-system :com.dvlsoft.clon)
+(load-system :cl-protobuf)
+(load-system :cl-spread)
+(load-system :cl-rsb)
+(map nil #'unintern '(for finally collect else with in)) ;; iterate bug
+
+(use-package :alexandria)
+(use-package :iter)
+(use-package :com.dvlsoft.clon)
+
+(defvar *client/server-test-uri* "spread:/rsbtest/clientserver")
+
+(defun main ()
+  (make-synopsis
+   :item (make-flag    :long-name   "help"
+		       :description "Display help text.")
+   :item (rsb:make-options))
+  (make-context)
+  (when (getopt :long-name "help")
+    (help)
+    (return-from main))
+
+  (format t "[Lisp    Client] Communicating with remote server at ~A~%"
+	  *client/server-test-uri*)
+
+  #+later
+  (rsb.patterns:with-remote-server (server *client/server-test-uri*)
+    ;; Test echo method.
+    (assert (string= (rsb.patterns:call server "echo" "bla") "bla"))
+
+    ;; Test error-producing method.
+    (let (signaled?)
+      (ignore-errors
+	(rsb.patterns:call server "error" "does not matter")
+	(setf signaled? t))
+      (unless signaled?
+	(error "~@<Method did not signal.~@:>")))
+
+    ;; Ask server to terminate.
+    (rsb.patterns:call server "terminate"))
+
+  (format t "[Lisp    Client] Done!~%"))
+
+(dump "client" main)
---
+++ lisp/server.lisp
@@ -0,0 +1,57 @@
+(load-system :sb-posix)
+(load-system :com.dvlsoft.clon)
+(load-system :cl-protobuf)
+(load-system :cl-spread)
+(load-system :cl-rsb)
+(map nil #'unintern '(for finally collect else with in)) ;; iterate bug
+
+(use-package :alexandria)
+(use-package :iter)
+(use-package :com.dvlsoft.clon)
+
+(defvar *client/server-test-uri* "spread:/rsbtest/clientserver")
+
+(defvar *terminate* (list (bt:make-lock)
+			  (bt:make-condition-variable)
+			  nil))
+
+(defun terminate-wait ()
+  (bt:with-lock-held ((first *terminate*))
+    (iter (until (third *terminate*))
+	  (bt:condition-wait (second *terminate*)))))
+
+(defun terminate-notify ()
+  (bt:with-lock-held ((first *terminate*))
+    (setf (third *terminate*) t)
+    (bt:condition-notify (second *terminate*))))
+
+(defun main ()
+  (make-synopsis
+   :item (make-flag    :long-name   "help"
+		       :description "Display help text.")
+   :item (rsb:make-options))
+  (make-context)
+  (when (getopt :long-name "help")
+    (help)
+    (return-from main))
+
+  (format t "[Lisp    Server] Providing service on ~A~%"
+	  *client/server-test-uri*)
+
+  #+later
+  (rsb.patterns:with-local-server (server *client/server-test-uri*)
+    (rsb.patterns:with-methods (server)
+	(("echo" (request string)
+	   (format t "[Lisp    Server] \"echo\" method called~%")
+	   request)
+	 ("error" (request string)
+	   (format t "[Lisp    Server] \"error\" method called~%")
+	   (error "intentional error"))
+	 ("terminate" (request string)
+	   (format t "[Lisp    Server] \"terminate\" method called~%")
+	   (terminate-notify)))
+      (terminate-wait)))
+
+  (format t "[Lisp    Server] done!~%"))
+
+(dump "client" main)
