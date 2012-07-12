4b1f0860a9e1b81659b7654ce2f8de0973b67d23
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2012-07-12 16:44:32 +0000

    Backport: Added package rsb.integration-test in lisp/*.lisp * lisp/common.lisp (header) updated copyright   (package rsb.integration-test): new package; for integration test   code * lisp/client.lisp (in-package rsb.integration-test): place code in   package rsb.integration-test * lisp/dump.lisp (in-package rsb.integration-test): likewise * lisp/informer.lisp (in-package rsb.integration-test): likewise * lisp/server.lisp (header): updated copyright   (in-package rsb.integration-test): place code in package   rsb.integration-test * lisp/listener.lisp (header): updated copyright   (in-package rsb.integration-test): place code in package   rsb.integration-test * lisp/event_id.lisp (header): updated copyright   (in-package rsb.integration-test): place code in package   rsb.integration-test * lisp/config.lisp (header): updated copyright   (in-package rsb.integration-test): place code in package   rsb.integration-test

--- lisp/informer.lisp
+++ lisp/informer.lisp
@@ -17,6 +17,8 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses>.
 
+(cl:in-package :rsb.integration-test)
+
 (defun informer-main ()
   ;; Commandline option boilerplate.
   (setf rsb:*default-configuration* (rsb:options-from-default-sources))
